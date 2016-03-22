9ebc9a507fe61eff5f3a7072b146786b4bb4cb46
Felipe Freitas <felipes@takenet.com.br>
2016-03-22 10:14:30 -0300

    fixed bug instance of interface by reflection in namespace

---
+++ src/Logging/ILoggerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Takenet.Library.Logging
+{
+    /// <summary>
+    /// Common interface for log listeners
+    /// </summary>
+    public interface ILoggerFactory
+    {
+        ILogger Create(string applicationName, IDictionary<string, string> propertyDictionary);
+
+        Type LoggerType { get; }
+    }
+}
